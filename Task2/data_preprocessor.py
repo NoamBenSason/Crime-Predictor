b5e1ab4e7beb9b34e5183ac1ca054e5c1740f19b
avishaiElmakies <36810152+avishaiElmakies@users.noreply.github.com>
2021-06-02 23:01:37 +0300

    start pre

---
+++ Task2/data_preprocessor.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+def preprocess_features(features):
+    pass
+
+
+def load_data():
+    df = pd.load_csv("Dataset_crimes_train.csv")
+    df = df[["Date", "Block", "Primary Type", "Location Description", "Arrest",
+             "Domestic", "Beat", "District", "Ward", "Community Area",
+             "X Coordinate", "Y Coordinate"]]
+
+    features, response = df.drop("Primary Type"), df["Primary Type"]
+    return
+
+
+if __name__ == '__main__':
+    load_data()
+
+
+
+
+
