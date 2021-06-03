514868962e5d5bcf17ad7d149231b90918027a07
avishaiElmakies <36810152+avishaiElmakies@users.noreply.github.com>
2021-06-03 14:41:58 +0300

    pre

--- Task2/data_preprocessor.py
+++ Task2/data_preprocessor.py
@@ -1,15 +1,19 @@
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 
-crimes_dict = {0: 'BATTERY', 1: 'THEFT', 2: 'CRIMINAL DAMAGE', 3: 'DECEPTIVE '
-                                                                  'PRACTICE',
+crimes_dict = {0: 'BATTERY', 1: 'THEFT', 2: 'CRIMINAL DAMAGE',
+               3: 'DECEPTIVE '
+                  'PRACTICE',
                4: 'ASSAULT'}
 crimes_dict_reverse = {'BATTERY': 0, 'THEFT': 1, 'CRIMINAL DAMAGE': 2,
                        'DECEPTIVE PRACTICE': 3, 'ASSAULT': 4}
 
 
 def preprocess_response(response):
-    process_response = response.apply(lambda x: crimes_dict_reverse.get(x))
+    process_response = response.apply(lambda x:
+                                      crimes_dict_reverse.get(
+                                          x))
     return process_response
 
 
@@ -21,24 +25,23 @@ def preprocess_features(features):
     features['time'] = features["Date"].dt.time
     features['time'] = features["time"].apply(lambda x: x.hour * 60 + x.minute)
     features['day_of_week'] = features["Date"].dt.dayofweek
-    features['block_no_street'] = features["Block"].str.slice(0, 6)
-    features = pd.get_dummies(features, prefix='block_',
-                              columns=['block_no_street'])
-    features = pd.get_dummies(features, prefix='location_',
-                              columns=['Location Description'])
-    features.drop(["Block", "Date"], axis=1, inplace=True)
+    features['block_no_street'] = features["Block"].str.slice(0, 5)
+    print(features.groupby("Location Description")["Location Description"].nunique().sort_values())
+
     return features
 
 
 def preprocess_all(features, response):
-    return preprocess_features(features), preprocess_response(response)
+    return preprocess_features(features), preprocess_response(
+        response)
 
 
 def load_data(filename):
     df = pd.read_csv(filename)
-    df = df[["Date", "Block", "Primary Type", "Location Description", "Arrest",
-             "Domestic", "Beat", "District", "Ward", "Community Area",
-             "X Coordinate", "Y Coordinate"]]
+    df = df[
+        ["Date", "Block", "Primary Type", "Location Description", "Arrest",
+         "Domestic", "Beat", "District", "Ward", "Community Area",
+         "X Coordinate", "Y Coordinate"]]
     df.dropna(inplace=True, axis=0)
 
     features, response = df.drop("Primary Type", axis=1), df["Primary Type"]
