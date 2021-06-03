a9b875d0c15556c05958aee5635da382a26dbc00
avishaiElmakies <36810152+avishaiElmakies@users.noreply.github.com>
2021-06-03 12:16:30 +0300

    pre

--- Task2/data_preprocessor.py
+++ Task2/data_preprocessor.py
@@ -7,6 +7,11 @@ crimes_dict_reverse = {'BATTERY': 0, 'THEFT': 1, 'CRIMINAL DAMAGE': 2,
                        'DECEPTIVE PRACTICE': 3, 'ASSAULT': 4}
 
 
+def preprocess_response(response):
+    process_response = response.apply(lambda x: crimes_dict_reverse.get(x))
+    return process_response
+
+
 def preprocess_features(features):
     features["Date"] = pd.to_datetime(features["Date"])
     features['day'] = features["Date"].dt.day
@@ -14,7 +19,12 @@ def preprocess_features(features):
     features['year'] = features["Date"].dt.year
     features['time'] = features["Date"].dt.time
     features['day_of_week'] = features["Date"].dt.dayofweek
-    return
+    features['block_no_street'] = features["Block"].str.slice(0, 6)
+    return features
+
+
+def preprocess_all(features, response):
+    return preprocess_features(features), preprocess_response(response)
 
 
 def load_data(filename):
@@ -22,13 +32,12 @@ def load_data(filename):
     df = df[["Date", "Block", "Primary Type", "Location Description", "Arrest",
              "Domestic", "Beat", "District", "Ward", "Community Area",
              "X Coordinate", "Y Coordinate"]]
+    df.dropna(inplace=True, axis=0)
 
     features, response = df.drop("Primary Type", axis=1), df["Primary Type"]
-    features.dropna(inplace=True,axis=0)
-    processed_features = preprocess_features(features)
 
-    return
+    return preprocess_all(features, response)
 
 
 if __name__ == '__main__':
-    load_data("Dataset_crimes_test.csv")
+    load_data("Dataset_crimes_train.csv")
